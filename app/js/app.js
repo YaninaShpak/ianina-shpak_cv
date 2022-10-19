f4708c1cfd44958af44f8d182f31c6cc306b0ffa
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-10-19 22:29:51 +0300

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,36 @@
+import { showElements } from "./modules/show-elements.js";
+import { showMenu } from "./modules/showMenu.js";
+import { toggleTHeme } from "./modules/toggle-theme.js";
+import { headerFixed } from "./modules/header-fixed.js";
+import { topBtnFixed } from "./modules/topBtn-fixed.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+  const icnMenu = document.querySelector('.burger-button');
+  const navMenu = document.querySelector('.main-nav');
+  const siteList = document.querySelector('.site-list');
+  const body = document.querySelector('body');
+  const header = document.querySelector('.main-header');
+  const intro = document.querySelector('.intro');
+  const topBtn = document.querySelector('.button-top');
+
+  showElements();
+  
+  showMenu(icnMenu, navMenu, body, siteList);
+  
+  toggleTHeme(body);
+
+  headerFixed(header);
+
+  topBtnFixed(intro, topBtn);
+
+  window.addEventListener('scroll', () => {
+    headerFixed(header);
+    topBtnFixed(intro, topBtn);
+  });
+
+  topBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+  });
+});
+
+
---
+++ gulp/config/path.js
@@ -0,0 +1,43 @@
+//Пути
+
+//Получаем имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`;
+const srcFolder = `./app`;
+
+export const path = {
+
+  build: {
+    js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    fonts: `${buildFolder}/fonts/`,
+    files: `${buildFolder}/files/`
+  },
+
+  src: {
+    js: `${srcFolder}/js/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
+    svgicons: `${srcFolder}/img/icons/social_icons/*.svg`,
+    files: `${srcFolder}/files/*.*`
+  },
+
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`
+  },
+  
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: ``
+}
